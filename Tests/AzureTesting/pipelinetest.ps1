e300446aa51f2a5d74e224af2b22f203b71ee577
Barbara <ba@4bes.nl>
2019-07-22 10:15:53 +0200

    Changed $script back to $global as it breaks test

--- Tests/AzureTesting/pipelinetest.ps1
+++ Tests/AzureTesting/pipelinetest.ps1
@@ -7,7 +7,7 @@ param(
 
 )
 
-$Script:TemplatePath = $TemplatePath
+$Global:TemplatePath = $TemplatePath
 $projectRoot = Resolve-Path "$PSScriptRoot\..\.."
 $moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
 $moduleName = Split-Path $moduleRoot -Leaf
