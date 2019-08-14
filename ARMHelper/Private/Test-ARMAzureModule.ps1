0301cf9836849066dd77c1f5c117414d81a6d759
Barbara Forbes <ba@4bes.nl>
2019-08-14 21:50:56 +0200

    New structure

--- ARMHelper/Private/Test-ARMAzureModule.ps1
+++ ARMHelper/Private/Test-ARMAzureModule.ps1
@@ -1,9 +1,12 @@
 Function Test-ARMAzureModule {
 
     $Module = $null
-    $AzureRM = Get-InstalledModule -Name AzureRM -ErrorAction SilentlyContinue
-    $Az = Get-InstalledModule -Name Az -ErrorAction SilentlyContinue
-    if (-not[string]::IsNullOrEmpty($Az)) {
+    $AzureRMLoaded = Get-Module -Name AzureRM.*
+    $AzLoaded = Get-Module -Name Az.*
+    $AzureRM = get-installedModule AzureRM -ErrorAction SilentlyContinue
+    $Az = Get-InstalledModule AZ -ErrorAction SilentlyContinue
+
+    if ((-not[string]::IsNullOrEmpty($Az)) -or (-not[string]::IsNullOrEmpty($AzLoaded)) ) {
         Write-Verbose "Az is found"
         try {
             $null = Get-AzContext
@@ -13,7 +16,7 @@ Function Test-ARMAzureModule {
         }
         $Module = "Az"
     }
-    elseif (-not[string]::IsNullOrEmpty($AzureRM)) {
+    Elseif ((-not[string]::IsNullOrEmpty($AzureRM)) -or (-not[string]::IsNullOrEmpty($AzureRMLoaded)) ) {
         Write-Verbose "AzureRM is found"
         try {
             $null = Get-AzureRmContext
@@ -23,9 +26,115 @@ Function Test-ARMAzureModule {
         }
         $Module = "AzureRM"
     }
-    else {
+    Else {
         Throw "neither AZ of AzureRM could be loaded"
     }
-
     $Module
+
+
 }
+# Function Test-ARMAzureModule {
+
+#     $Module = $null
+#      # First, check if the module is already loaded
+#     $AzureRMLoaded = Get-Module -Name AzureRM.*
+#     $AzLoaded = Get-Module -Name Az.*
+
+#     $AzureRM = Get-InstalledModule -Name AzureRM -ErrorAction SilentlyContinue
+#     $Az = Get-InstalledModule -Name Az -ErrorAction SilentlyContinue
+#     if ((-not[string]::IsNullOrEmpty($Az)) -or (-not[string]::IsNullOrEmpty($AzLoaded)) ) {
+#         Write-Verbose "Az is found"
+#         try {
+#             $null = Get-AzContext
+#         }
+#         Catch {
+#             Throw "No connection with Az has been found. Please Connect."
+#         }
+#         $Module = "Az"
+#     }
+#     elseif ((-not[string]::IsNullOrEmpty($AzureRM)) -or (-not[string]::IsNullOrEmpty($AzurermLoaded))) {
+#         Write-Verbose "AzureRM is found"
+#         try {
+#             $null = Get-AzureRmContext
+#         }
+#         Catch {
+#             Throw "No connection with AzureRM has been found. Please Connect."
+#         }
+#         $Module = "AzureRM"
+#     }
+#     else {
+#         Throw "neither AZ of AzureRM could be loaded"
+#     }
+
+#     $Module
+# }
+# Function Test-ARMAzureModule {
+
+#     $Module = $null
+
+#     # First, check if the module is already loaded
+#     $AzureRMLoaded = Get-Module -Name AzureRM.*
+#     $AzLoaded = Get-Module -Name Az.*
+
+#     if (-not[string]::IsNullOrEmpty($AzLoaded)) {
+#         Write-Verbose "Az is found"
+#         $Module = "Az"
+#         Continue
+#     }
+#     else {
+#         $Az = Get-InstalledModule -Name Az -ErrorAction SilentlyContinue
+#         if (-not[string]::IsNullOrEmpty($Az)) {
+#             Write-Verbose "Az is found, but not imported. Importing now"
+#             Try {
+#                 Import-Module Az -force
+#                 $Module = "Az"
+#                 Continue
+#             }
+#             Catch {
+#                 Throw "Could not import Module Az"
+#             }
+#         }
+#         elseif (-not[string]::IsNullOrEmpty($AzureRMLoaded)) {
+#             Write-Verbose "AzureRM is found"
+#             $Module = "AzureRM"
+#             Continue
+#         }
+#         else {
+#             $AzureRM = Get-InstalledModule -Name AzureRM -ErrorAction SilentlyContinue
+#             if (-not[string]::IsNullOrEmpty($AzureRM)) {
+#                 Write-Verbose "AzureRM is found, but not imported. Importing now"
+#                 Try {
+#                     Import-Module AzureRM -force
+#                     $Module = "AzureRM"
+#                     Continue
+#                 }
+#                 Catch {
+#                     Throw "Could not import Module AzureRM"
+#                 }
+#             }
+#         }
+#     }
+
+#     if ($Module -eq "Az") {
+#         Try {
+#             Write-Verbose "GetazContext"
+#             $null = Get-AzContext
+#         }
+#         Catch {
+#             Throw "No connection with $Module has been found. Please Connect."
+#         }
+#     }
+#     elseif ($Module -eq "AzureRM") {
+#         Try {
+#             Write-Verbose "GetazurermContext"
+#             $null = Get-AzureRMContext
+#         }
+#         Catch {
+#             Throw "No connection with $Module has been found. Please Connect."
+#         }
+#     }
+#     else {
+#         Throw  "neither AZ of AzureRM could be loaded"
+#     }
+#     $Module
+# }
--- Tests/Test-ARMAzureModule.tests.ps1
+++ Tests/Test-ARMAzureModule.tests.ps1
@@ -5,11 +5,19 @@ $moduleName = Split-Path $moduleRoot -Leaf
 Import-Module (Join-Path $moduleRoot "$moduleName.psd1") -force
 
 
-Describe 'When AzureRM is available' -Tag @("Mock") {
+Describe 'Test-ARMAzureModule' -Tag @("Mock") {
     InModuleScope ARMHelper {
-        Context 'Basic functionality' {
+        Context 'When module is not loaded' {
+            It "If none are available, script should throw" {
+                Mock Get-InstalledModule -MockWith { $null }
+                Mock Get-Module -MockWith { $null }
+                { Test-ARMAzureModule } | Should -Throw "neither AZ of AzureRM could be loaded"
+
+            }
             function Get-AzContext  ([String]$Name, [Object]$Value, [Switch]$Clobber) { }
             function Get-AzureRMContext([String]$Name, [Object]$Value, [Switch]$Clobber) { }
+            Mock Get-Module { $null }
+            Mock Import-Module { $null }
             Mock Get-InstalledModule -MockWith {
                 [pscustomobject]@{
                     "Version"     = "6.13.1"
@@ -22,7 +30,7 @@ Describe 'When AzureRM is available' -Tag @("Mock") {
                 $null
             } -ParameterFilter { $Name -eq "Az" }
             Mock Get-AzureRmContext -MockWith { "no error" }
-
+            Mock Get-AzContext -MockWith { "no error" }
             It "If Only AzureRM is available, output should be AzureRM" {
                 $Result = Test-ARMAzureModule
                 $Result | Should -be "AzureRM"
@@ -39,6 +47,7 @@ Describe 'When AzureRM is available' -Tag @("Mock") {
                         "Description" = "Azure Resource Manager Module"
                     }
                 } -ParameterFilter { $Name -eq "Az" }
+                Mock Import-Module -MockWith { "no error" }
                 Mock Get-AzContext -MockWith { "no error" }
 
                 $Result = Test-ARMAzureModule
@@ -64,40 +73,48 @@ Describe 'When AzureRM is available' -Tag @("Mock") {
                 $Result = Test-ARMAzureModule
                 $Result | Should -be "Az"
             }
-            It "If none are available, script should throw" {
+        }
+        Context 'When module is loaded' {
+            function Get-AzContext  ([String]$Name, [Object]$Value, [Switch]$Clobber) { }
+            function Get-AzureRMContext([String]$Name, [Object]$Value, [Switch]$Clobber) { }
+
+            Mock Get-AzureRmContext -MockWith { "no error" }
+            It "If AzureRM is already loaded, output should be AzureRM" {
                 Mock Get-InstalledModule -MockWith {
                     $null
-                } -ParameterFilter { $Name -eq "Az" }
+                }
+                Mock Get-Module -MockWith {
+                    $null
+                } -ParameterFilter { $Name -eq "Az.*" }
+                Mock Get-Module -MockWith {
+                    [array]@(
+                        "module1",
+                        "Module2"
+                    )
+                } -ParameterFilter { $Name -eq "AzureRM.*" }
+                $Result = Test-ARMAzureModule
+                $Result | Should -be "AzureRM"
+            }
+            It "If Az is already loaded, output should be Az" {
                 Mock Get-InstalledModule -MockWith {
                     $null
-                } -ParameterFilter { $Name -eq "AzureRM" }
+                }
+                Mock Get-Module -MockWith {
+                    $null
+                } -ParameterFilter { $Name -eq "AzureRM.*" }
+                Mock Get-Module -MockWith {
+                    [array]@(
+                        "module1",
+                        "Module2"
+                    )
+                } -ParameterFilter { $Name -eq "Az.*" }
 
-                { Test-ARMAzureModule } | Should -Throw "neither AZ of AzureRM could be loaded"
+                Mock Get-AzContext -MockWith { "no error" }
 
+                $Result = Test-ARMAzureModule
+                $Result | Should -be "Az"
             }
-            It "If Connection with Azure can't be made, script should throw" {
-                Mock Get-InstalledModule -MockWith {
-                    [pscustomobject]@{
-                        "Version"     = "6.13.1"
-                        "Name"        = "AzureRM"
-                        "Repository"  = "PSGallery"
-                        "Description" = "Azure Resource Manager Module"
-                    }
-                } -ParameterFilter { $Name -eq "AzureRM" }
-                Mock Get-AzureRMContext -MockWith { Throw "error" }
-                { Test-ARMAzureModule } | Should -Throw "No connection with AzureRM has been found. Please Connect."
 
-                Mock Get-InstalledModule -MockWith {
-                    [pscustomobject]@{
-                        "Version"     = "2.4.0"
-                        "Name"        = "Az"
-                        "Repository"  = "PSGallery"
-                        "Description" = "Azure Resource Manager Module"
-                    }
-                } -ParameterFilter { $Name -eq "Az" }
-                Mock Get-AzContext -MockWith { Throw "error" }
-                { Test-ARMAzureModule } | Should -Throw "No connection with Az has been found. Please Connect."
-            }
         }
     }
 }
