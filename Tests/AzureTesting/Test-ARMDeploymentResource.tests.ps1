9c96c08984787fcc1b995d8c865b7176180603d5
Barbara 4bes <43912004+Ba4bes@users.noreply.github.com>
2019-10-12 15:34:49 +0200

    Workaround for #37 (#39)
    This pull request handles the fact that the output for Test-AzResourceGroupDeployment has changed.
    While not all functionality is restored, this does fix all errors.
    As this issue only exists in the Az module, a warning is given with the advice to temporary use the AzureRM module

--- Tests/AzureTesting/Test-ARMDeploymentResource.tests.ps1
+++ Tests/AzureTesting/Test-ARMDeploymentResource.tests.ps1
@@ -16,8 +16,17 @@ Describe 'Check Test-ARMDeploymentResource with Azure' -Tag @("Az") {
         }
         Context 'Basic functionallity' {
 
+            it "Gives a warning for using Az-module"{
+                $Result = (Test-ARMDeploymentResource @Parameters 3>&1)
+                if ($Module -eq "Az"){
+                $Result.Message[0] | Should -BeLike "The AZ-module is used. This limits the results of this cmdlet*"
+            }
+            if ($Module -eq "AzureRM"){
+                $Result.Message[0] | Should -Not -BeLike "The AZ-module is used. This limits the results of this cmdlet*"
+            }
+        }
             It "Works with a parameterFile" {
-                { Test-ARMDeploymentResource @Parameters } | Should -Not -Throw
+                { Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue } | Should -Not -Throw
             }
             It "works with a parameter object" {
                 $Parameterobject = @{
@@ -30,7 +39,7 @@ Describe 'Check Test-ARMDeploymentResource with Azure' -Tag @("Az") {
                     templatefile            = "$PSScriptRoot\VirtualMachine\azuredeploy.json"
                     templateparameterobject = $Parameterobject
                 }
-                { Test-ARMDeploymentResource @Parameters } | Should -Not -Throw
+                { Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue } | Should -Not -Throw
 
             }
             It "works with added Parameters" {
@@ -41,13 +50,14 @@ Describe 'Check Test-ARMDeploymentResource with Azure' -Tag @("Az") {
                     adminUsername     = "Test"
                     dnsLabelPrefix    = "eandomrlkajelnadada"
                 }
-                { Test-ARMDeploymentResource @Parameters } | Should -Not -Throw
+                { Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue } | Should -Not -Throw
             }
             It "When a deployment is correct, script doesn't throw" {
-                { Test-ARMDeploymentResource @Parameters } | Should -Not -Throw
+                { Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue } | Should -Not -Throw
             }
             It "Shows standard properties for resources that would be in the deployment" {
-                $Result = Test-ARMDeploymentResource @Parameters
+                $Result = Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue
+                if ($Module -eq "AzureRM"){
                 $Result[0].Resource | Should -be "StorageAccounts"
                 $Result[0].Name | Should -be "mstaqkc32c2qmmw"
                 $Result[0].Type | Should -be "Microsoft.Storage/storageAccounts"
@@ -56,23 +66,37 @@ Describe 'Check Test-ARMDeploymentResource with Azure' -Tag @("Az") {
                 $Result[0].ID | Should -BeLike "*/resourceGroups/ArmHelper/providers/Microsoft.Storage/storageAccounts/mstaqkc32c2qmmw"
                 $Result[0].kind | Should -be "Storage"
                 $Result[0].sku.name | Should -BeNullOrEmpty
+                }
+                if ($Module -eq "Az"){
+                    $Result[0].Resource | Should -be "StorageAccounts"
+                    $Result[0].Name | Should -be "mstaqkc32c2qmmw"
+                    $Result[0].Type | Should -be "Microsoft.Storage/storageAccounts"
+                    $Result[0].ID | Should -BeLike "*/resourceGroups/ArmHelper/providers/Microsoft.Storage/storageAccounts/mstaqkc32c2qmmw"
+
+                }
             }
+            if ($Module -eq "AzureRM"){
             It "Shows all properties when using Select-Object *" {
-                $Result = Test-ARMDeploymentResource @Parameters | Select-Object *
+                if ($Module -eq "AzureRM"){
+                $Result = Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue | Select-Object *
                 $result[0].'.sku.name' | Should -be "Standard_LRS"
                 $Result[0].apiVersion | Should -be "2018-11-01"
+
             }
             it "When a parameter is a securestring, it is shown as a securestring in the output" {
-                $Result = Test-ARMDeploymentResource @Parameters
+
+                $Result = Test-ARMDeploymentResource -WarningAction SilentlyContinue @Parameters
                 $Result[-1].adminPassword | Should -Be "System.Security.SecureString"
+                }
             }
+        }
             it "Should throw when no result is found" {
                 $Parameters = @{
                     resourcegroupname     = "ArmHelper"
                     templatefile          = "$PSScriptRoot\StorageAccountBroken\azuredeploy.json"
                     templateparameterfile = "$PSScriptRoot\StorageAccountBroken\azuredeploy.parameters.json"
                 }
-                { Test-ARMDeploymentResource @Parameters } | Should throw "Something is wrong with the output, no resources found. Please check your deployment with Get-ARMdeploymentErrorMessage"
+                { Test-ARMDeploymentResource @Parameters -WarningAction SilentlyContinue } | Should throw "Something is wrong with the output, no resources found. Please check your deployment with Get-ARMdeploymentErrorMessage"
             }
         }
     }
